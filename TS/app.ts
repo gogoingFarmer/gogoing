b352cee24f9434dfa9926becfa98668073f5d4f6
farmer <onecodefarmer@163.com>
2019-09-04 21:27:02 +0800

    ts

---
+++ TS/app.ts
@@ -0,0 +1,16 @@
+class Greeter{
+    greeting:string;
+    constructor(message:string){
+        this.greeting = message;
+    }
+    greet(){
+        return 'hello, ' + this.greeting;
+    }
+}
+let greeter = new Greeter('world');
+let button = document.createElement('button');
+button.textContent = 'Say Hello';
+button.onclick = function(){
+    alert(greeter.greet())
+}
+document.body.appendChild(button)

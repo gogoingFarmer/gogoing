52ff4e8453038d2504d08c3d472b5b65b0dafd34
alone <alone@163.com>
2019-10-07 15:52:14 +0800

    1

--- react16/basic/src/App.js
+++ react16/basic/src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react' // ES6写法解构赋值
+/*
+    ES5
+    import React from 'react'
+    const Component = React.Component 
+*/
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+    render(){
+        return(
+            <div className='index'>
+                index page
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
